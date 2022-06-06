72675c4eed3d80655f93af0e40b036afcea1b4f4
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-06-06 18:01:00 +0200

    Adding objets to levels

--- scripts/script.js
+++ scripts/script.js
@@ -146,6 +146,7 @@ function defineObjects() {
         ['fresco', false],
         ['soporte', false],
         ['baul', false],
+        ['llave', false],
         ['ladrillo', false],
         ['cuerda', false],
         ['cerradura', false],
@@ -168,79 +169,79 @@ function initMap() {
         objects: ['ascuas', 'carbon'],
     };
     levels[0][1] = {
-        description: 'El interior de la piramide está oscuro. Palpando la pared tocas algo que parece una antorcha.',
+        description: 'El interior de la piramide está oscuro. Palpando la pared tocas algo que parece una ANTORCHA.',
         exits: ['e','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['antorcha', 'lino'],
     };
     levels[0][2] = {
         description: 'Al avanzar por el pasillo, la luz de la antorcha ilumina las paredes llenas de jeroglíficos.',
         exits: ['e','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['mapa'],
     };
     levels[0][3] = {
         description: 'Te encuentras de frente con una pared. A lo lejos aún se puede ver la luz tenue de la entrada. El pasillo parece girar a la derecha.',
         exits: ['s', 'o'],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[1][0] = {
         description: 'Junto a la pared hay una estatua de oro macizo.',
         exits: ['e','s'],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[1][1] = {
         description: 'Al adentrarte en la bóveda descubres un baul de madera maciza con incrustraciones de oro.',
         exits: ['e','s','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['baul', 'llave'],
     };
     levels[1][2] = {
         description: 'Al cruzar la puerta, accedes a un gran recibidor. El eco de tus pasos indica que estás en una especie de bóveda. En las paredes hay frisos de oro. Parece que los ladrones no han conseguido llegar hasta aquí.',
         exits: ['e','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[1][3] = {
         description: 'Al girar, entras en un vestíbulo. Frente a tí hay un fresco de Anubis que llega hasta el techo. A tu derecha hay una puerta custodiada por una estatua del dios Ra.',
         exits: ['n','s','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['estatua', 'baston', 'fresco'],
     };
     levels[2][0] = {
         description: 'Hay una puerta de madera maciza con una cerradura de hierro. Junto a ella, una cuerda cuelga de la pared.',
         exits: ['n','e','s'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['cuerda'],
     };
     levels[2][1] = {
         description: 'Hay una pared de piedra repleta de jeroglíficos.',
         exits: ['n','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['ladrillo'],
     };
     levels[2][2] = {
         description: 'La habitación está repleta de tesoros. En el centro de la sala hay un féretro de oro iluminado por un rayo de sol que entra por una claraboya. Felicidades. Tu búsqueda ha terminado. Parece que después de tantos años, al fin has encontrado la tumba de Tutankamon',
         exits: [],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[2][3] = {
         description: 'Al atravesar la puerta escondida tras el fresco, accedes a una habitación secreta. Al igual que el resto de la pirámide, las paredes están llenas de jeroglíficos. Desde donde estás no consigues ver lo que hay al fondo de la habitación.',
         exits: ['n','s'],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[3][0] = {
         description: 'El suelo está lleno de escombros. Hay piedras por todas partes y trozos de madera humeante.',
         exits: ['n','e'],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[3][1] = {
         description: 'Los escombros llegan hasta aquí. En el suelo hay un amasijo de hierro retorcido de lo que debió ser la cerradura.',
         exits: ['e','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: [],
     };
     levels[3][2] = {
         description: 'Hay una puerta de madera maciza con una cerradura de hierro.',
         exits: ['n','o'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['cerradura'],
     };
     levels[3][3] = {
         description: 'Al adentrarte más en la habitación secreta, observas un pequeño altar. Tras él, hay un soporte cilíndrico anclado a la pared.',
         exits: ['n'],
-        objects: ['ascuas', 'carbon'],
+        objects: ['soporte'],
     };
 
     return levels;
